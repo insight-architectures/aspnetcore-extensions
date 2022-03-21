eac41e92ee17617878fb92e789d8a542b26fa898
Renato Golia <renato.golia@insightarchitectures.com>
2022-03-21 17:12:42 +0100

    Added GrpcServiceMethodConfigurationExtensions

---
+++ tests/Tests.Grpc.ChannelConfiguration/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Tests.Grpc.ChannelConfiguration;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
